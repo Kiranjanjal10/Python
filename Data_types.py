8d256892195d772e2e728239506cfe9c91232473
Kiran janjal <kiranjanjal10@gmail.com>
2026-07-31 17:37:25 +0530

    Initial Commit

--- Data_types.py
+++ Data_types.py
@@ -8,3 +8,32 @@ print(type(Integer_data))
 print(type(Float_data))
 print(type(Boolean_data))
 print(type(None_data))
+
+a=2
+print(a)
+
+b=24234235346546546
+print(b)
+
+c=45.67
+print(c)
+
+d="kiran"
+print(d)
+
+e='a'
+print(e)
+
+f=True
+print(f)
+
+g=None
+print(g)
+
+a, b, _ = 1, 2, 3
+print(a,b, _)
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+
